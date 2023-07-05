3d7d26dd0a2c6edd8fdbc3f2ab002808b9846395
Muhammadamin <webhacker0100@gmail.com>
2023-07-05 21:53:37 +0500

    Created new Modal tsx page

--- src/pages/admin/Modal.tsx
+++ src/pages/admin/Modal.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Cards = () => {
+
+    const [modal, setModal] = useState<Boolean>(false)
+
+    function updateModal(){
+        setModal(!modal)
+    }
     return (
-        <h1>Hello world!</h1>
+        <div>
+             <h3 className="text-3xl font-medium text-gray-700">
+      Modal
+    </h3>
+    <button
+      className="px-6 py-3 mt-3 font-medium tracking-wide text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none"
+      onClick={updateModal}
+    >
+      Open Modal
+    </button>
+
+        </div>
     )
 }
 
