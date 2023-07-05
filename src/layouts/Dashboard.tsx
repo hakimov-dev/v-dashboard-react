5d136ad3b8f010956b4a383d43970870fd81570c
Muhammadamin <webhacker0100@gmail.com>
2023-07-05 15:35:08 +0500

    Add main content in header component

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
 interface CardProps {
   children: ReactNode;
@@ -10,7 +11,7 @@ const Dashboard: React.FC<CardProps> = ({children}) => {
     <div className="flex h-screen bg-gray-200 font-roboto">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-          {/* Headers place */}
+         <Header/>
 
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
           <div className="container mx-auto px-6 py-8">
