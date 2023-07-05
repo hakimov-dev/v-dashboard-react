501bb376a6ec0cd5841bfb8d61cfe56c9ed49c0f
Muhammadamin <webhacker0100@gmail.com>
2023-07-05 19:55:47 +0500

    Added main content in Tables page

--- src/pages/admin/Tables.tsx
+++ src/pages/admin/Tables.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useTableData } from "../../composables/tableData";
 
 const Tables = () => {
-    return (
-        <h1>Hello world!</h1>
-    )
-}
+  const { simpleTableData, paginatedTableData, wideTableData } = useTableData();
 
-export default Tables
+  return (
+    <div>
+      <h3 className="text-3xl font-medium text-gray-700">Tables</h3>
+
+      <div className="mt-4">
+      <h4 className="text-gray-600">
+        Simple Table
+      </h4>
+
+      <div className="mt-6">
+        <div className="my-6 overflow-hidden bg-white rounded-md shadow">
+          <table className="w-full text-left border-collapse">
+            <thead className="border-b">
+              <tr>
+                <th
+                  className="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
+                >
+                  City
+                </th>
+                <th
+                  className="px-5 py-3 text-sm font-medium text-gray-100 uppercase bg-indigo-800"
+                >
+                  Total orders
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+               {simpleTableData.map((i) => (
+                <tr
+                className="hover:bg-gray-200"
+              >
+                <td className="px-6 py-4 text-lg text-gray-700 border-b">
+                  { i.city }
+                </td>
+                <td className="px-6 py-4 text-gray-500 border-b">
+                  { i.totalOrders }
+                </td>
+              </tr>
+               ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Tables;
